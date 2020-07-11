23a1cfa0d5f07b3208e5fb25e6ddbbbbe06010f6
Ryosuke Sugisawa <mainac2@gmail.com>
2020-07-11 23:40:28 +0900

    Sample

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>サンプル作成</h1>
+    <input v-model="myname" placeholder="入力してください">
+    <p>私は、{{ myname }}です</p>
+    <p>上記文字列は{{myname.Length }}です</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      myname: 'テスト文字列',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
